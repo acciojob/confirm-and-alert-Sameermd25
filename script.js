dd4d4e2e61086cac55dce687f042447bd77ecc56
Sameermd25 <149755097+Sameermd25@users.noreply.github.com>
2025-11-06 22:11:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const userConfirmed = confirm("Do you want to proceed?");
+
+// Check the user's response
+if (userConfirmed) {
+  alert("You clicked OK. Proceeding...");
+} else {
+  alert("You clicked Cancel. Exiting...");
+}
